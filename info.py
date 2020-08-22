0a701517c4e5ba85ae92cda5c5aa00e51ee3ed20
sarvesh <sksonkar88@gmail.com>
2020-08-22 16:22:09 +0530

    First info file

---
+++ info.py
@@ -0,0 +1,10 @@
+import sys
+print(sys.executable)
+import sys
+print(sys.executable)
+//Test your imports
+import cv2
+import numpy as np
+import matplotlib
+//Display cv2 version
+print cv2._version_
