5d40d891deb3869718f5016b6077eca452802e8f
sarvesh <sksonkar88@gmail.com>
2020-08-23 00:33:29 +0530

    Covert gray scal image

---
+++ gray.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Load our input image
+image = cv2.imread('./images/input.jpg')
+cv2.imshow('Original', image)
+cv2.waitKey()
+
+# We use cvtColor, to convert to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Grayscale', gray_image)
+cv2.waitKey()
+cv2.destroyAllWindows()
