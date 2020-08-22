d2cfc127e8a3b336c8da314cd7561b870ead3b08
sarvesh <sksonkar88@gmail.com>
2020-08-22 23:24:57 +0530

    Basic of Image

---
+++ Image_basic.py
@@ -0,0 +1,13 @@
+# Code:
+# Sarvesh Kumar Sonkar
+# OpenCV takes a couple seconds to import the first time
+import cv2
+# We use as np, so that everything we call on numpy, we can type np instead
+import numpy as np
+# Load an image using 'imread' specifying the path to image
+input = cv2.imread('./images/input.jpg')
+# Load an image using 'imread' specifying the path to image
+input = cv2.imread('./images/input.jpg')
+# how long you keep the window open (time is in milliseconds here)
+cv2.waitKey()
+cv2.destroyAllWindows()
